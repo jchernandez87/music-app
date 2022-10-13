25c1ea0347b5289c7740bfee7647152328b6c57a
Juan Carlos <jchernandez827@gmail.com>
2022-10-12 20:42:07 -0500

    add sidebar cuntionality and styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,34 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((link) => (
+      <NavLink
+        className="flex flex-row justify-start items-start my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        key={link.name}
+        to={link.to}
+        onClick={() => handleClick && handleClick()}
+      >
+        <link.icon className="w-6 h-6 mr-2" />
+        {link.name}
+      </NavLink>
+    ))}
+  </div>
 );
+
+const Sidebar = () => {
+  const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
+
+  return (
+    <div className="hidden md:flex flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <NavLinks />
+    </div>
+  );
+};
 
 export default Sidebar;
