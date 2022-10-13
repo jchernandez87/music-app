e70239346e71e17cd8054db5ce700e8e006d1c8f
Juan Carlos <jchernandez827@gmail.com>
2022-10-12 20:15:22 -0500

    add music player functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPLaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'pop';
   const currentGenres = genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>);
@@ -26,7 +29,14 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPLaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
         ))}
       </div>
     </div>
