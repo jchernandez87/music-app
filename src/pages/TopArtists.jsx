c8d63a5303f39c31bb9d4b3a6dde46126d0c30a0
Juan Carlos <jchernandez827@gmail.com>
2022-10-17 17:32:58 -0500

    implement ArtistCard component

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,28 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { ArtistCard, Loader, Error } from '../components';
+import Track from '../components/MusicPlayer/Track';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
-export default TopArtists;
+const TopArtist = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading top charts" />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Top Artists
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard
+            key={Track.key}
+            track={track}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopArtist;
