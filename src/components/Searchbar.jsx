21d409f3ff9c2fa047994274bbc0639766803727
Juan Carlos <jchernandez827@gmail.com>
2022-10-17 21:02:33 -0500

    add border to searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -12,7 +12,7 @@ const Searchbar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className="pt-2 text-gray-400 focus-within:text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className="border-8 border-red-500 pt-2 text-gray-400 focus-within:text-gray-600">
       <label htmlFor="search-field" className="sr-only">
         Search all songs
       </label>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
-      <div className="absolute block top-6 right-3">
+      <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
           <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
         ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} /> }
